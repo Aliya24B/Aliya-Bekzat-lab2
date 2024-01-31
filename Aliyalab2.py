bbb93bf1fdce3d3e6aeff72be1e2fb0c46cd354b
Aliya24B <158310296+Aliya24B@users.noreply.github.com>
2024-01-31 22:32:48 +0600

    Add files via upload

---
+++ Aliyalab2.py
@@ -0,0 +1,91 @@
+#ex1
+print("Hello World")
+
+#ex2
+if 5 > 2:
+    print("YES")
+
+#ex3
+#This is a comment
+
+#eX4
+""""
+This is a comment
+written in 
+more than just one line
+"""
+
+#ex5
+carname = "Volvo"
+
+#ex6
+x = 50 
+
+#ex7
+x = 5
+y = 10
+print(x+y)
+
+#ex8
+x = 5
+y = 10
+z = x+y
+print(z)
+
+#ex9
+x, y, z = "Orange", "Banana", "Cherry"
+
+#ex10
+x = y = z = "Orange"
+
+#ex11
+def myfunc():
+    global x
+    x = "fantastic"
+
+#ex12
+x = 5
+print(type(x))
+int
+
+#ex13
+x = "Hello World"
+print(type(x))
+str
+
+#ex14
+x = 20.5
+print(type(x))
+float
+
+#ex15
+x = ["apple", "banana", "cherry"]
+print(type(x))
+list
+
+#ex16
+x = ("apple", "banana", "cherry")
+print(type(x))
+tuple
+
+#ex17
+x = {"name" : "John", "age" : 36}
+print(type(x))
+dict
+
+#ex18
+x = True
+print(type(x))
+bool
+
+#ex19
+x = 5
+x = float(x)
+
+#ex20
+x = 5.5
+x = int(x)
+
+#ex21
+x = 5
+x = complex(x)
